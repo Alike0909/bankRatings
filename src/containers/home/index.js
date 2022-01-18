6e91b90d3a7fdf7106cc02d41e58ab1f9a740e13
Alnur <will.silver01@gmail.com>
2022-01-19 05:59:20 +0600

    last commit

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Table } from 'antd';
+import { Table, Popover } from 'antd';
 import './index.css'
 
 import { collection, getDocs } from "firebase/firestore";
@@ -30,11 +30,13 @@ function Home() {
             title: "Наименование банка",
             dataIndex: "name",
             fixed: 'left',
+            width: 135.5,
         },
         {
             title: "Размер $млн",
             dataIndex: "capitalization",
             defaultSortOrder: 'descend',
+            width: 94.8,
             sorter: {
                 compare: (a, b) => a.capitalization - b.capitalization,
                 multiple: 4,
@@ -44,10 +46,14 @@ function Home() {
         {
             title: "Владелец",
             dataIndex: "owner",
+            width: 125,
+            render: name => getOwner(name),
         },
         {
             title: "Рейтинг форума",
             dataIndex: "forumRate",
+            defaultSortOrder: 'descend',
+            width: 99,
             sorter: {
                 compare: (a, b) => {
                     if (rate.indexOf(b.forumRate) > rate.indexOf(a.forumRate)) {
@@ -64,12 +70,13 @@ function Home() {
         {
             title: "Рейтинг S&P500, Fitch, Moody's",
             dataIndex: "journalRate",
+            width: 102,
             sorter: {
                 compare: (a, b) => {
-                    if (rate.indexOf(b.forumRate) > rate.indexOf(a.forumRate)) {
+                    if (rate.indexOf(b.journalRate) > rate.indexOf(a.journalRate)) {
                         return 1;
                     }
-                    if (rate.indexOf(b.forumRate) < rate.indexOf(a.forumRate)) {
+                    if (rate.indexOf(b.journalRate) < rate.indexOf(a.journalRate)) {
                         return -1;
                     }
                     return 0;
@@ -80,23 +87,38 @@ function Home() {
         {
             title: "Живые деньги",
             dataIndex: "realMoney",
+            width: 92,
             sorter: {
                 compare: (a, b) => a.realMoney.replace(/%/, "") - b.realMoney.replace(/%/, ""),
                 multiple: 8,
             },
+            render: number => (
+                Number(number.replace(/%/, "")) > 30 ?
+                    <span style={{ color: 'green' }}>{number}</span>
+                    :
+                    <span style={{ color: 'red' }}>{number}</span>
+            ),
         },
         {
             title: "Живые деньги (min)",
             dataIndex: "realMoneyMin",
+            width: 92,
             sorter: {
                 compare: (a, b) => a.realMoneyMin.replace(/%/, "") - b.realMoneyMin.replace(/%/, ""),
                 multiple: 6,
             },
+            render: number => (
+                Number(number.replace(/%/, "")) > 30 ?
+                    <span style={{ color: 'green' }}>{number}</span>
+                    :
+                    <span style={{ color: 'red' }}>{number}</span>
+            ),
         },
         {
             title: "Дыра капитала",
             dataIndex: "capitalHole",
             defaultSortOrder: 'ascend',
+            width: 106,
             sorter: {
                 compare: (a, b) => a.capitalHole.replace(/%/, "") - b.capitalHole.replace(/%/, ""),
                 multiple: 5,
@@ -111,34 +133,62 @@ function Home() {
         {
             title: "Качество займов",
             dataIndex: "loanQuality",
+            width: 106,
             sorter: {
                 compare: (a, b) => a.loanQuality - b.loanQuality,
                 multiple: 4,
             },
+            render: number => (
+                number < 60 ?
+                    <span style={{ color: 'green' }}>{number}</span>
+                    :
+                    <span style={{ color: 'red' }}>{number}</span>
+            ),
         },
         {
             title: "Аппетит к риску",
             dataIndex: "appetiteRisk",
+            width: 99,
             sorter: {
                 compare: (a, b) => a.appetiteRisk.replace(/%/, "") - b.appetiteRisk.replace(/%/, ""),
                 multiple: 3,
             },
+            render: number => (
+                Number(number.replace(/%/, "")) < 70 ?
+                    <span style={{ color: 'green' }}>{number}</span>
+                    :
+                    <span style={{ color: 'red' }}>{number}</span>
+            ),
         },
         {
             title: "Доля рынка",
             dataIndex: "marketShare",
+            width: 85,
             sorter: {
                 compare: (a, b) => a.marketShare.replace(/%/, "") - b.marketShare.replace(/%/, ""),
                 multiple: 9,
             },
+            render: number => (
+                Number(number.replace(/%/, "")) > 2 ?
+                    <span style={{ color: 'green' }}>{number}</span>
+                    :
+                    <span style={{ color: 'red' }}>{number}</span>
+            ),
         },
         {
             title: "Доля ритейла",
             dataIndex: "retailShare",
+            width: 98,
             sorter: {
                 compare: (a, b) => a.retailShare.replace(/%/, "") - b.retailShare.replace(/%/, ""),
                 multiple: 4,
             },
+            render: number => (
+                Number(number.replace(/%/, "")) > 20 ?
+                    <span style={{ color: 'green' }}>{number}</span>
+                    :
+                    <span style={{ color: 'red' }}>{number}</span>
+            ),
         },
         {
             title: "Бумажные",
@@ -146,6 +196,7 @@ function Home() {
                 {
                     title: "NPL",
                     dataIndex: "npl",
+                    width: 70,
                     sorter: {
                         compare: (a, b) => a.npl.replace(/%/, "") - b.npl.replace(/%/, ""),
                         multiple: 6,
@@ -154,6 +205,7 @@ function Home() {
                 {
                     title: "Капитал",
                     dataIndex: "capital",
+                    width: 99,
                     sorter: {
                         compare: (a, b) => a.capital.replace(/%/, "") - b.capital.replace(/%/, ""),
                         multiple: 2,
@@ -162,6 +214,7 @@ function Home() {
                 {
                     title: "Профит",
                     dataIndex: "profit",
+                    width: 96,
                     sorter: {
                         compare: (a, b) => a.profit.replace(/%/, "") - b.profit.replace(/%/, ""),
                         multiple: 2,
@@ -173,6 +226,7 @@ function Home() {
             title: "Активы",
             dataIndex: "assets",
             defaultSortOrder: 'descend',
+            width: 93,
             sorter: {
                 compare: (a, b) => a.assets - b.assets,
                 multiple: 4,
@@ -187,6 +241,7 @@ function Home() {
         {
             title: "Вклады",
             dataIndex: "deposits",
+            width: 94,
             sorter: {
                 compare: (a, b) => a.deposits - b.deposits,
                 multiple: 4,
@@ -201,6 +256,7 @@ function Home() {
     ]
 
     const [data, setData] = useState([])
+    const [owner, setOwner] = useState([])
 
     async function fetchBanks() {
         const banksRef = collection(db, "banks");
@@ -208,6 +264,45 @@ function Home() {
         querySnapshot.forEach((doc) => {
             setData(prev => [...prev, doc.data()]);
         });
+    }
+
+    async function fetchOwners() {
+        const ownersRef = collection(db, "owners");
+        const querySnapshot = await getDocs(ownersRef);
+        querySnapshot.forEach((doc) => {
+            setOwner(prev => [...prev, doc.data()]);
+        });
+    }
+
+    function getOwner(name) {
+        let flag = false
+        let ownerName = null
+        let officialName = null
+        for (let i in owner) {
+            if (owner[i].id == name) {
+               flag = true
+                ownerName = owner[i].name
+                officialName = owner[i].official
+            } 
+        }
+
+        if (flag === true) {
+            return (
+                <Popover content={ownerName}>
+                    <span className="ownerName" style={{ display: 'flex', alignItems: 'center' }}>
+                        {officialName}
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="#1890ff" class="bi bi-info-circle" viewBox="0 0 16 16">
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                            <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+                        </svg>
+                    </span>
+                </Popover>
+            )
+        } else {
+            return (
+                <span>{name}</span>
+            )
+        }
     }
 
     function onChange(pagination, filters, sorter, extra) {
@@ -235,11 +330,48 @@ function Home() {
 
     useEffect(() => {
         fetchBanks()
+        fetchOwners()
     }, [])
 
     return (
         <div className="home">
-            <Table columns={columns} dataSource={data} onChange={onChange} showSorterTooltip={false} pagination={false}/>
+            <Table 
+                columns={columns} 
+                dataSource={data} 
+                onChange={onChange} 
+                showSorterTooltip={false} 
+                pagination={false} 
+                scroll={{ y: 700 }}
+                summary={pageData => {
+                    let totalCapitalization = 0;
+                    let totalRealMoney = 0;
+                    let totalRealMoneyMin = 0;
+                    let totalLoanQuality = 0;
+                    pageData.forEach((item) => {
+                        totalCapitalization += Number(item.capitalization);
+                        totalRealMoney += Number(item.realMoney.replace(/%/, ""));
+                        totalRealMoneyMin += Number(item.realMoneyMin.replace(/%/, ""));
+                        totalLoanQuality += Number(item.loanQuality);
+                    })
+                    return (
+                        <Table.Summary fixed>
+                            <Table.Summary.Row>
+                                <Table.Summary.Cell>Все банки</Table.Summary.Cell>
+                                <Table.Summary.Cell>{thousandSeparator(totalCapitalization)}</Table.Summary.Cell>
+                                <Table.Summary.Cell></Table.Summary.Cell>
+                                <Table.Summary.Cell></Table.Summary.Cell>
+                                <Table.Summary.Cell></Table.Summary.Cell>
+                                <Table.Summary.Cell>{(totalRealMoney / pageData.length).toFixed(1)}%</Table.Summary.Cell>
+                                <Table.Summary.Cell>{(totalRealMoneyMin / pageData.length).toFixed(1)}%</Table.Summary.Cell>
+                                <Table.Summary.Cell></Table.Summary.Cell>
+                                <Table.Summary.Cell>{(totalLoanQuality / pageData.length).toFixed(1)}</Table.Summary.Cell>
+                                <Table.Summary.Cell></Table.Summary.Cell>
+                                <Table.Summary.Cell>100%</Table.Summary.Cell>
+                            </Table.Summary.Row>
+                        </Table.Summary>
+                    )
+                }}
+            />
         </div>
     )
 }
